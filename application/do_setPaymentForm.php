e3d993ca0693019825e3f4b1bcb9041ec79846c4
farlistener <contact@levieuxcedric.com>
2015-04-21 20:35:12 +0200

    Ajout d'un filtre xss

--- application/do_setPaymentForm.php
+++ application/do_setPaymentForm.php
@@ -1,8 +1,30 @@
-<?php
+<?php /*
+	Copyright 2015 Cédric Levieux, Parti Pirate
+
+	This file is part of PPMoney.
+
+    PPMoney is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PPMoney is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PPMoney.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 include_once("config/database.php");
 include_once("config/paybox.php");
 require_once("engine/bo/ProjectBo.php");
 require_once("engine/bo/TransactionBo.php");
+require_once("engine/utils/FormUtils.php");
+
+// We sanitize the request fields
+xssCleanArray($_REQUEST);
 
 if (!isset($_REQUEST["iCertify"])) exit();
 
