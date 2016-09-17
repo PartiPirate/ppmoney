b96115162244afcc1e736498d00a7a13c217243d
farlistener <contact@levieuxcedric.com>
2016-09-17 11:33:26 +0200

    #13
    Remove link on active page

--- application/donate.php
+++ application/donate.php
@@ -24,7 +24,7 @@ include_once("header.php");
 <div class="gradient-black-invisible pull-right" style="width: 100px;">
 	<div class="bg-white" style="height: 20px; margin: 0 0 0 1px; width: 99px;"></div>
 	<div class="bg-white text-center" style="margin-bottom: 0; padding: 0; border: 1px solid black; border-right-color: #B0B0B0; border-top-color: #B0B0B0;"><a href="https://adhesion.partipirate.org/join.php">Adhérer</a></div>
-	<div class="bg-white text-center" style="margin-bottom: 0; padding: 0; border: 1px solid black; border-top: 0; border-left-color: white;"><a href="https://don.partipirate.org/donate.php">Donner</a></div>
+	<div class="bg-white text-center" style="margin-bottom: 0; padding: 0; border: 1px solid black; border-top: 0; border-left-color: white;">Donner</div>
 	<div class="bg-white" style="height: 100px; margin: 0 0 0 1px; width: 99px;"></div>
 </div>
 
